6657ccc10967e2146325079be32a9d90454efb62
Carl Placek <carl.placek@icloud.com>
2025-11-28 11:39:42 +0100

    aufgaben von stunden 10 und 11

--- 10/klassenmodel.py
+++ 10/klassenmodel.py
@@ -1,159 +1,176 @@
-#Modellierung eines Rechnungsbelegs 
-#get = lesen , set = verändern
 import datetime as dt
 
-class Invoice: 
-    def __init__(self): 
-        self._nummer = self._set_number()
-        self._datum = dt.date.today()
+# Rechnung
+class Invoice:
+    tax_rate = 0.19 #Klassenatribbut ist für alle Instanzen gleich, keine Konstante (in Großbuchstaben), da es auch geändert werden kann
+
+    def __init__(self):
+        self._number = self._set_number() #darf nicht von außen mitgenommen werden (unikate nummer), __ = private, darf nicht vererbt werden
+        self._date = dt.date.today()
         self._items = []
-       
-        
-    def __str__(self): 
-        invoice_string = f"\nRechnungsnr: {self._nummer} Rechnungsdatum: {self._datum}"
-        
-        invoice_string += f"\n\nPositionen:" 
-        for item in self._items:
-            invoice_string += f"\n{item.__str__()}"
-        return invoice_string
-    
-    
-    def _set_number(self): 
-        return 187
-        
-    def _get_new_items_id(self):
-        try: 
-            max_id = max(item.get_item_id() for item in self._items)
+        self._net_total = 0
+        self._tax = 0
+        self._total = 0
+        self._payment_type = None  #Muss vom Aufrufenden gesetzt werden
+
+    def _set_number(self): # _ = proteced, nicht von außen zugreifen
+        # vorübergehende Lösung: Eine Rechnungsnummer muss eindeutig sein
+        return 42
+
+    def _get_new_item_id(self):
+        try:
+            max_id = max(item.get_id() for item in self._items) # id wird aufsteigend zugeteilt, max id definiert
             return max_id + 1
         except:
             return 1
-    
-    def add_items(self, description, quantity, net_price_per_unit): 
+
+    def add_item(self, description, quantity, net_price_per_unit):
         item = Item(
-            items_id = self._get_new_items_id(),
-            description = description,
-            quantity = quantity,
-            net_price_per_unit = net_price_per_unit
+            id=self._get_new_item_id(),
+            description=description,
+            quantity=quantity,
+            net_price_per_unit=net_price_per_unit
         )
         self._items.append(item)
-        
+        # Update der Totals nach Hinzufügen einer neuen Position erforderlich
+        self._calculate_totals()
+
+    def get_date(self):
+        return self._date
+
     def get_items(self):
-        return self._items.copy()
+        return self._items.copy()  #flache Kopie
 
+    def _calculate_totals(self):
+        # Initialisierung der Werte (relevant bei erneuter Berechnung)
+        self._net_total = 0
+        self._tax = 0
+        self._total = 0
+        for item in self._items:
+            self._net_total += item.get_net_total()
 
+        self._tax = self._net_total * Invoice.tax_rate
+        self._total = self._net_total + self._tax
+
+    def set_payment_type(self, payment_type):
+        self._payment_type = payment_type
+
+    def __str__(self):
+        # Überschrift
+        invoice_string = f"Rechnung {self._number}\nRechnungsdatum {self._date}"
+        #Positionen
+        invoice_string += f"\n\nPositionen:"
+        for item in self._items:
+            invoice_string += f"\n{item.__str__()}"
+        # Totals
+        # :.f sorgt für eine Ausgabe mit 2 Nachkommastellen (gerundet wird mit Banker's Rounding)
+        invoice_string += f"\n\nZwischensumme (netto): {self._net_total:.2f}" \
+                          f"\nSteuer: {self._tax:.2f}" \
+                          f"\nGesamtbetrag: {self._total:.2f}"
+        # Zahlungsbedingungen
+        invoice_string += f"\n\nZahlungsbedingungen\n" \
+                          f"{self._payment_type.get_payment_terms()}"
+
+        return invoice_string
     
 
-class Item():
-    def __init__(self, items_id, description, quantity, net_price_per_unit,): 
-        self._id = items_id 
-        self._description = description 
+# Positionen
+class Item:
+    def __init__(self, id, description, quantity, net_price_per_unit):
+        self._id = id
+        self._description = description
         self._quantity = quantity
         self._net_price_per_unit = net_price_per_unit
-        self._net_total = self._quantity * self._net_price_per_unit 
-        self._total_price = self._net_total * 1.19
-    
-    def get_item_id(self): 
+        self._net_total = self._quantity * self._net_price_per_unit
+
+    def get_id(self):
         return self._id
-    
-    
+
+    def get_net_total(self):
+        return self._net_total
+
     def __str__(self):
-        return f"{self._id} | {self._description} | {self._net_price_per_unit} | {self._net_total} | {self._total_price}\n"
+        return f"{self._id} {self._description} {self._net_price_per_unit} {self._net_total}"
+    
+
+# Zahlungsarten
+class PaymentType:
+    def get_payment_terms(self):
+        # Muss in Subklassen implementiert werden
+        # Durch Vererbung gibt es die gemeinsame Schnittstelle/Methode get_payment_terms
+        return None
 
 
-invoice_bsp = Invoice()
-invoice_bsp.add_items(
-    description = "Porsche 964 dunkelgrün",
-    quantity= 2, 
-    net_price_per_unit= 80000
+
+# Überweisung
+class BankTransfer(PaymentType):
+    def __init__(self, iban, due_date, account_holder):
+        # Aufruf von __init__ der Superklasse hier noch irrelevant,
+        # bei zukünftiger Erweiterung der Superklasse aber ggf. wichtig
+        self._iban = iban
+        self._due_date = due_date  #Fälligkeitsdatum
+        self._account_holder = account_holder  #Kontoinhaber
+
+    def get_payment_terms(self):
+        # könnte natürlich auch in der __str__-Methode implementiert werden.
+        # get_payment_terms ist allerdings ein verständlicherer Name
+        payment_terms = f"Bitte überweisen Sie den Gesamtbetrag " \
+                        f"bis zum {self._due_date} " \
+                        f"auf das Konto\n\n" \
+                        f"{self._account_holder}\n" \
+                        f"IBAN: {self._iban}"
+        return payment_terms
+
+
+
+class PayPal(PaymentType):
+    def get_payment_terms(self):
+        return "Betrag bereits mit PayPal beglichen."
+    
+invoice_bank_transfer = Invoice()
+invoice_bank_transfer.add_item(
+    description="Kyberkristall (für Lichtschwert)",
+    quantity=1,
+    net_price_per_unit=3000
 )
-invoice_bsp.add_items(
-    description = "Ferrari f40 rot",
-    quantity= 1, 
-    net_price_per_unit= 450000
+invoice_bank_transfer.add_item(
+    description="Wartung Lichtschwert (Reinigung, Justage)",
+    quantity=1,
+    net_price_per_unit=350
 )
-invoice_bsp.add_items(
-    description = "VW Polo schwarz",
-    quantity= 3, 
-    net_price_per_unit= 12000
+invoice_bank_transfer.add_item(
+    description="Düsentriebwerk für T-16 Skyhopper",
+    quantity=2,
+    net_price_per_unit=1299
 )
 
-print(invoice_bsp)
+invoice_bank_transfer.set_payment_type(
+    BankTransfer(
+        iban="DE12 3456 7890 1234 5678 00",
+        due_date=invoice_bank_transfer.get_date() + dt.timedelta(weeks=2),  # Fälligkeitsdatum 2 Wochen später
+        account_holder="Imperiale Technikhandel GmbH"
+    )
+)
 
-"""for item in invoice_bsp.get_items():
-    print(f"{item.get_item_id()}| {item._description}| {item._net_price_per_unit}| {item._net_total}") 
-"""
+print(invoice_bank_transfer)
 
+invoice_paypal = Invoice()
+invoice_paypal.add_item(
+    description="Kyberkristall (für Lichtschwert)",
+    quantity=1,
+    net_price_per_unit=3000
+)
+invoice_paypal.add_item(
+    description="Wartung Lichtschwert (Reinigung, Justage)",
+    quantity=1,
+    net_price_per_unit=350
+)
+invoice_paypal.add_item(
+    description="Düsentriebwerk für T-16 Skyhopper",
+    quantity=2,
+    net_price_per_unit=1299
+)
 
-class Kunde: 
-    def __init__(self, nummer, name, rechnungsadresse):
-        self.nummer = nummer
-        self.name = name 
-        self.rechnungsadresse = rechnungsadresse
+invoice_paypal.set_payment_type(PayPal())
 
-class Rechnungssteller(): 
-    def __init__(self, ust_id, adresse):
-        self.ust_id = ust_id
-        self.adresse = adresse 
-        
-
-
-class Zahlungsart(): 
-    def __init__(self, name): 
-        self.name = name
-    
-    def bezahlen(self): 
-        raise NotImplementedError()
-    
-    def __str__(self): 
-        return f"Zahlungsart: {self.name}"
-
-
-class Ueberweisung(Zahlungsart): 
-    def __init__(self, iban, name):
-        super().__init__(name) #self wird schon übernommen
-        self.iban = iban
-    
-    def bezahlen(self, betrag): 
-        return f"{betrag} wurde via Überweisungskonto {self.iban} gesendet"
-    
-    
-
-class Kreditkarte(Zahlungsart): # methoden in  oop und funktionen in funktionsorientierprog.
-    def __init__(self, nummer, name):
-        super().__init__(name) 
-        self.nummer = nummer
-    
-    def bezahlen(self, betrag): 
-        return f"{betrag} wurde via Kreditkarte {self.nummer} gesendet"
-       
-
-class Paypal(Zahlungsart): 
-    def __init__(self, email, name):
-        super().__init__(name) 
-        self.email = email
-       
-    
-    def bezahlen(self, betrag):
-        return f"{betrag} wurde via PayPal {self.email} gesendet"
-
-
-
-zahlung1 = Ueberweisung(name="carl", iban="123445755")
-print(zahlung1.bezahlen(betrag=1234))
-
-zahlung2 = Kreditkarte(name="otto", nummer="538393462")
-print(zahlung2.bezahlen(betrag=238000))
-
-zahlung2 = Paypal(name="gustav", email="gustav.servus@icloud.com")
-print(zahlung2.bezahlen(betrag=300000))    
-
-
-
-class Absender: 
-    def __init__(self):
-        pass
-    
-class Empfaenger: 
-    def __init__(self):
-        pass
-    
+print(invoice_paypal)
---
+++ 11/static_method.py
@@ -0,0 +1,57 @@
+class BankAccount: #Klassenmethode
+    zinsen = 0.03 
+    number_of_accounts = 0
+    bank_name = "Rothschild"
+    
+    def __init__(self, inhaber, kontostand): 
+        self._inhaber = inhaber 
+        self._kontostand = kontostand
+        BankAccount.number_of_accounts += 1
+        
+    
+    def deposit(self, amount):
+        """Guthaben aufladen"""
+        if amount <= 0:
+            raise ValueError("Beitrag muss positiv sein!")
+        self._kontostand -= amount
+        return self._kontostand
+    
+    def withdraw(self, amount): 
+        """Guthaben abheben"""
+        if amount <= 0:
+            raise ValueError("Beitrag muss positiv sein!")
+        if amount > self._kontostand: 
+            raise ValueError("Nicht genügend Guthaben")
+        self._kontostand += amount
+        return self._kontostand
+        
+        
+    def calculate_interest(self): 
+        """Zinsen auf das Konto anwenden"""
+        zinsen = self._kontostand * BankAccount.zinsen
+        self._kontostand += zinsen
+        return zinsen
+    
+    
+    @staticmethod
+    def is_amount_valid(amount): 
+        """Einfache Validierung"""
+        return amount > 0
+     
+    @classmethod
+    def get_number_of_accounts(cls): 
+        return cls.number_of_accounts
+    
+    @classmethod
+    def set_bank_name(cls): 
+        return cls.bank_name
+    
+#manuell testen 
+konto1 = BankAccount("Tom", 3000)
+konto1.deposit(100)
+konto1.withdraw(300)
+zinsen = konto1.calculate_interest()
+bank_name = konto1.set_bank_name()
+
+    
+print(f"Inhaber: {konto1._inhaber}\nKontostand: {konto1._kontostand}\nZinsen: {zinsen}\nBankname: {bank_name}")
